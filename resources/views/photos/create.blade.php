0c2df538c14cbac03a6a4f38880789ca451cde59
cat214 <hayasaka3314@gmail.com>
2019-10-24 05:11:00 +0900

    フォーム修正

--- resources/views/photos/create.blade.php
+++ resources/views/photos/create.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <h1 class="post-title">写真を投稿する</h1>
-<form>      
-  <input name="name" type="text" class="feedback-input" placeholder="タイトル" />   
-  <textarea name="text" class="feedback-input" placeholder="説明"></textarea>
-  <input type="file" class="feedback-input" >
+<form action="/index" method="post" ectype="multipart/form-data">      
+  <input name="title" type="text" class="feedback-input" placeholder="タイトル" />   
+  <textarea name="text" name="description" class="feedback-input" placeholder="説明"></textarea>
+  <input type="file" name="photo" class="feedback-input" >
   <input type="submit" value="投稿""/>
 </form>
 @endsection
