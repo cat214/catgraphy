be519c7e6d24a9d465af30c95594d69b586d9cb8
cat214 <hayasaka3314@gmail.com>
2019-10-23 18:06:43 +0900

    認証機能追加

---
+++ resources/views/inc/topbar.blade.php
@@ -0,0 +1,9 @@
+<div class="top-bar">
+    <ul　class="nav">
+        <h1 class="top-bar-list">catgraphy</h1>
+        <li class="top-bar-list"><a href="/index">ホーム</a></li>
+        <li class="top-bar-list"><a href="/create">投稿する</a></li>
+        <li class="top-bar-list"><a href="/login">ログイン</a></li>
+        <li class="top-bar-list"><a href="/mypage">マイページ</a></li>
+    </ul>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index',function(){
+    return view('layouts.app');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
