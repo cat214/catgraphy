1829f58057d5a29da4f08a7f15267e9801d3c034
cat214 <hayasaka3314@gmail.com>
2019-11-01 05:22:31 +0900

    レスポンシブ

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <ul>
         @foreach($photos as $photo)
-            <div class="col-md-6">
+            <div class="col-md-6 col-xs-12">
             <li class="content-list"> 
                 <a href="/photos/{{$photo->id}}" class="content-link">
                     <img src="storage/photos/{{$photo->thumbnail}}" class="content-image">
