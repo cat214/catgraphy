9051b47b6d31c78cff97f39002b3269500978f51
cat214 <hayasaka3314@gmail.com>
2019-11-01 05:20:21 +0900

    レスポンシブ化

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.app')
 @section('content')
 @isset($photos)
-<div id="grid">
-    <ul>
+<div id="grid" class="container">
+    <div class="row">
+        <ul>
         @foreach($photos as $photo)
+            <div class="col-md-6">
             <li class="content-list"> 
                 <a href="/photos/{{$photo->id}}" class="content-link">
                     <img src="storage/photos/{{$photo->thumbnail}}" class="content-image">
                     <p class="content-title">{{$photo->title}}</p>
                 </a>
             </li>
+            </div>
         @endforeach
-    </ul>
+        </ul>
+    </div>
 <div>
 <div class="pagination">
         {{ $photos->links() }}
